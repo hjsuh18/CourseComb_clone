b0ee69f4e85edaa7e3d628d60aba06fad6989166
AdaNguyenx <linhchib.nguyen@gmail.com>
2018-04-01 09:47:59 -0400

    implement primitive search

--- test/courses/views.py
+++ test/courses/views.py
@@ -3,9 +3,18 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Course
 
 def home(request):
-	courses = Course.objects.all()
-	return render(request, 'home.html', {'courses': courses})
+	# return render(request, 'home.html')
+	if 'searchform' in request.GET:
+	    searchinput = request.GET.get("searchinput", "")
+	    print searchinput
+	    responseobject = {
+	    	'message': searchinput
+	    }
+	    return JsonResponse(responseobject)
+	else:
+		# courses = Course.objects.all()
+		return render(request, 'home.html')
