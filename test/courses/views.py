f2bf81861e43fcf9588be6e5daff3545aede6af5
AdaNguyenx <linhchib.nguyen@gmail.com>
2018-04-10 21:48:10 -0400

    add delete function

--- test/courses/views.py
+++ test/courses/views.py
@@ -33,9 +33,19 @@ def home(request):
 			new_faves = curr_profile.faves + "," + registrar_id
 			curr_profile.faves = new_faves
 			curr_profile.save()
-			responseobject = {'newclass': class_name}
+			responseobject = {'newclass': class_name, 'newid': registrar_id}
 		else:
 			responseobject = {}
+		return JsonResponse(responseobject)
+	# delete course from course queue
+	elif 'deleteclass' in request.POST:
+		registrar_id = request.POST.get("registrar_id", "")
+		favorites = curr_profile.faves
+		favorites = favorites.split(",")
+		curr_faves = [x for x in favorites if registrar_id not in x]
+		curr_profile.faves = ','.join(curr_faves)
+		curr_profile.save()
+		responseobject = {}
 		return JsonResponse(responseobject)
 	else:
 		favorites = curr_profile.faves
@@ -44,7 +54,7 @@ def home(request):
 		for i in favorites:
 			if (i != ''):
 				course = Course.objects.filter(registrar_id = i)
-				curr_faves.append(course[0].deptnum + ": " + course[0].title)
+				curr_faves.append("<li class = '" + i + "'>" + course[0].deptnum + ": " + course[0].title + " <button type = 'button' class = 'btn btn-danger btn-xs deleteclass' id = '" + i + "'> x </button> </li>") 
 		return render(request, 'home.html', {"favorites": curr_faves})
 
 def get_courses(request):
