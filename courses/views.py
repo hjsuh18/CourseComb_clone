52a31d8f0e4222b2b03d198034e8769529fd6fa8
AdaNguyenx <linhchib.nguyen@gmail.com>
2018-04-26 01:10:26 -0400

    add precept selection

--- courses/views.py
+++ courses/views.py
@@ -132,15 +132,17 @@ def home(request):
 		d = dict(request.POST)
 		f = Filter.objects.update_or_create(
 			user = curr_profile,
-			must_courses = d.get("courses[]"),
-			must_dept = d.get("depts[]"),
-			distribution = d.get("distribution[]"),
-			max_dept = int(d.get("max_dept")[0]),
-			no_friday_class = (d.get("no_friday_class")[0] == 'true'),
-			no_evening_class = (d.get("no_evening_class")[0] == 'true'),
-			ten_am = (d.get("ten_am")[0] == 'true'),
-			full = (d.get("full")[0] == 'true'),
-			pdf = (d.get("pdf")[0] == 'true'),
+			defaults = {
+				'must_courses': d.get("courses[]"),
+				'must_dept': d.get("depts[]"),
+				'distribution': d.get("distribution[]"),
+				'max_dept': int(d.get("max_dept")[0]),
+				'no_friday_class': (d.get("no_friday_class")[0] == 'true'),
+				'no_evening_class': (d.get("no_evening_class")[0] == 'true'),
+				'ten_am': (d.get("ten_am")[0] == 'true'),
+				'full': (d.get("full")[0] == 'true'),
+				'pdf': (d.get("pdf")[0] == 'true'),
+			}
 			)
 
 		filter_course(curr_profile)
@@ -189,7 +191,6 @@ def home(request):
 	# show schedule of selected combination
 	# LOOKS LIKE THE PLACE TO IMPLEMENT TIME FILTERS/RESOLVE DISPLAY OF TIME CONFLICTS
 	elif 'comb_click' in request.GET:
-		print curr_profile.filter
 		full_filter = curr_profile.filter.full
 		no_friday_class = curr_profile.filter.no_friday_class
 		no_evening_class = curr_profile.filter.no_evening_class
@@ -250,7 +251,8 @@ def home(request):
 						continue
 					days = day_convert(m.days)
 					newdays = [i+1 for i, j in enumerate(days) if j == 1]
-					class_schedule = {'title': course.deptnum + " " + m.section, 'dow': newdays, 'start': m.start_time, 'end':m.end_time, 'color': lightpalette[int(registrar_id)%10]}
+					class_schedule = {'title': course.deptnum + " " + m.section, 'dow': newdays, 'start': m.start_time, 'end':m.end_time, 'color': lightpalette[int(registrar_id)%10], 'className':
+					'precept_render', 'id': course.deptnum + "-" + m.section}
 					course_classes_schedule.append(class_schedule)
 			responseobject[course.deptnum] = json.dumps(course_classes_schedule, default=str)
 
